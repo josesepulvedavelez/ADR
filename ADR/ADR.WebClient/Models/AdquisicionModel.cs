921dda8b259f12c834abbf6013b92f5c3e1dcc15
josesepulvedavelez <40329991+josesepulvedavelez@users.noreply.github.com>
2024-06-07 12:03:09 -0500

    Estructura de proyecto Front
    Estructura de proyecto Front

---
+++ ADR/ADR.WebClient/Models/AdquisicionModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADR.WebClient.Models
+{
+    public class AdquisicionModel
+    {
+        public decimal Presupuesto { get; }
+        public string Unidad { get; set; }
+        public string TipoBien { get; set; }
+        public int Cantidad { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal { get; set; }
+        public DateTime FechaAdquisicion { get; set; }
+        public string Proveedor { get; set; }
+        public string Documentacion { get; set; }
+
+        [Key]
+        public int AdquisicionId { get; set; }
+    }
+}
